0d10d7d78081dc334bfad22f346c40678afb72a9
Jay <jeremie.st.jules.prevost@gmail.com>
2016-11-19 14:20:09 -0500

    functionning script

--- script/ThrusterControl.py
+++ script/ThrusterControl.py
@@ -6,41 +6,55 @@ from provider_keypad.msg import Keypad
 from sonia_msgs.srv import SendCanMessage
 from sonia_msgs.srv import SendCanMessageRequest
 from sonia_msgs.srv import SendCanMessageResponse
+import time
 
 from std_msgs.msg import String
+# send_can_msg  = None
+class ThrusterControl:
+    def __init__(self):
+        self.node = None
+        self.values = list()
+        self.request_device_id = SendCanMessageRequest.DEVICE_ID_actuators
+        self.request_unique_id = [SendCanMessageRequest.UNIQUE_ID_ACT_port_motor, SendCanMessageRequest.UNIQUE_ID_ACT_starboard_motor,
+                                  SendCanMessageRequest.UNIQUE_ID_ACT_back_depth_motor, SendCanMessageRequest.UNIQUE_ID_ACT_front_depth_motor,
+                                  SendCanMessageRequest.UNIQUE_ID_ACT_back_heading_motor, SendCanMessageRequest.UNIQUE_ID_ACT_front_heading_motor]
+        self.request_method_number = SendCanMessageRequest.METHOD_MOTOR_set_speed
 
-def keypad_callback(data):
-    try:
-        send_can_msg = rospy.ServiceProxy('/provider_can/send_can_message', SendCanMessage)
-        
-        request_device_id = SendCanMessageRequest.DEVICE_ID_actuators
-        request_unique_id = [SendCanMessageRequest.UNIQUE_ID_ACT_port_motor, SendCanMessageRequest.UNIQUE_ID_ACT_starboard_motor,
-                             SendCanMessageRequest.UNIQUE_ID_ACT_back_depth_motor, SendCanMessageRequest.UNIQUE_ID_ACT_front_depth_motor,
-                             SendCanMessageRequest.UNIQUE_ID_ACT_back_heading_motor, SendCanMessageRequest.UNIQUE_ID_ACT_front_heading_motor]
-        request_method_number = SendCanMessageRequest.METHOD_MOTOR_set_speed
-        value = [(float(data.LJ_Up + data.LJ_Down) / 127.0)*100.0, (float(data.RJ_Up + data.RJ_Down) / 127.0)*100.0,
-                 (data.RT/254.0)*100.0, (data.LT/254.0)*100.0,
-                 (float(data.RJ_Left + data.RJ_Right) / 127.0)*100.0, (float(data.LJ_Left + data.LJ_Right) / 127.0)*100.0]
+    def keypad_callback(self, data):
+        self.values = [(float(data.LJ_Up + data.LJ_Down) / 127.0)*100.0, (float(data.RJ_Up + data.RJ_Down) / 127.0)*100.0,
+                       (data.RT/254.0)*100.0, (data.LT/254.0)*100.0,
+                       (float(data.RJ_Left + data.RJ_Right) / 127.0)*100.0, (float(data.LJ_Left + data.LJ_Right) / 127.0)*100.0]
 
-        for i in range(len(request_unique_id)):
-            """device_id, unique_id, method_number, parameter_value"""
-            resp1 = send_can_msg(request_device_id, request_unique_id[i], request_method_number, value[i])
 
-    except rospy.ServiceException, e:
-        print "Service call failed: %s"%e
+    def listener(self):
 
-def listener():
+        # In ROS, nodes are uniquely named. If two nodes with the same
+        # node are launched, the previous one is kicked off. The
+        # anonymous=True flag means that rospy will choose a unique
+        # name for our 'listener' node so that multiple listeners can
+        # run simultaneously.
+        rospy.init_node('thruster_control', anonymous=True)
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('thruster_control', anonymous=True)
+        rospy.Subscriber("/provider_keypad/Keypad", Keypad, self.keypad_callback)
 
-    rospy.Subscriber("/provider_keypad/Keypad", Keypad, keypad_callback)
+        self.node = rospy.ServiceProxy('/provider_can/send_can_message', SendCanMessage)
+        self.node(2,1,0,50)
+        time.sleep(2)
+        self.node(2,1,0,0)
+        while(1):
+            time.sleep(0.1)
+            try:
+                for i in range(len(self.values)):
+                    """device_id, unique_id, method_number, parameter_value"""
+                    print "rosservice call /provider_can/send_can_message "+str(self.request_device_id) + " " \
+                          + str(self.request_unique_id[i]) + " " \
+                          + str(self.request_method_number) + " " \
+                          + str(self.values[i])
+                    self.node(self.request_device_id, self.request_unique_id[i], self.request_method_number, self.values[i])
 
-    rospy.spin()
+            except rospy.ServiceException, e:
+                print "Service call failed: %s"%e
 
 if __name__ == '__main__':
-    listener()
+    tmp = ThrusterControl()
+tmp.listener()

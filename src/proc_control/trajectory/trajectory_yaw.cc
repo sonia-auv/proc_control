ea4f80e45b2b3adfe967ea3aa70505d641e1ce34
fmassey <francis.masse05@gmail.com>
2017-06-25 14:19:18 -0400

    Trajectory yaw stub

---
+++ src/proc_control/trajectory/trajectory_yaw.cc
@@ -0,0 +1,43 @@
+/**
+ * \file	trajectory_yaw.cc
+ * \author Francis Masse <francis.masse05@gmail.com>
+ * \date	25/06/17
+ *
+ * \copyright Copyright (c) 2017 S.O.N.I.A. AUV All rights reserved.
+ *
+ * \section LICENSE
+ *
+ * This file is part of S.O.N.I.A. software.
+ *
+ * S.O.N.I.A. AUV software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * S.O.N.I.A. AUV software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with S.O.N.I.A. AUV software. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "trajectory_yaw.h"
+
+//==============================================================================
+// C / D T O R S   S E C T I O N
+
+//------------------------------------------------------------------------------
+//
+TrajectoryYaw::TrajectoryYaw() {}
+
+//------------------------------------------------------------------------------
+//
+TrajectoryYaw::~TrajectoryYaw() {}
+
+//==============================================================================
+// M E T H O D   S E C T I O N
+
+//-----------------------------------------------------------------------------
+//

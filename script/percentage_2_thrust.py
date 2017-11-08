9f9084e76c55d4d35b4d0f363bb497943abcd42e
olavoie <olavoie9507@gmail.com>
2017-11-07 23:42:14 -0500

    adding publisher to log thrust

--- script/percentage_2_thrust.py
+++ script/percentage_2_thrust.py
@@ -1,32 +1,49 @@
+#!/usr/bin/env python
 import rospy
 
 from interface_rs485.msg import SendRS485Msg
+from proc_control.msg import ThrustLogging
+import struct
+import numpy as np
 
 
 class Percentage2Thrust:
 
     def __init__(self):
+        rospy.init_node('transform_thrust', anonymous=True)
         self.THRUSTs = []
         self.thrust_percentage_sub = rospy.Subscriber('/interface_rs485/dataRx', SendRS485Msg, self._thrust_percentage_callback)
+        self.thrust_publisher = rospy.Publisher('/proc_control/thrust_logging', ThrustLogging, queue_size=100)
+        self.thruster = np.array([[0.7071, -0.7071, 0, 0, 0, -0.25],
+                                  [-0.7071, -0.7071, 0, 0, 0, 0.25],
+                                  [-0.7071, 0.7071, 0, 0, 0, -0.25],
+                                  [0.7071, 0.7071, 0, 0, 0, 0.25],
+                                  [0, 0, -1, 0.25, 0.5, 0],
+                                  [0, 0, -1, 0.5, -0.5, 0],
+                                  [0, 0, -1, -0.5, -0.5, 0],
+                                  [0, 0, -1, -0.5, 0.5, 0]])
+        self.i = 0
+        rospy.spin()
 
     def _thrust_percentage_callback(self, msg):
-        if msg.slave == SendRS485Msg.SLAVE_ISI_PWM:
-            percentage = msg.data
+        percentage = list(struct.unpack("{}B".format(len(msg.data)), msg.data))
+        if len(percentage) == 8:
             self.percentage_2_pwm(percentage)
 
     def percentage_2_pwm(self, percentage):
         PWMs = []
         for percent in percentage:
             if percent > 0 and percent < 100:
-                pwm = 3.7 * percent + 1100
+                PWMs.append(int(3.7 * percent + 1100))
             elif percent <= 200 and percent > 100 :
-                pwm = 3.7 * percent + 1160
+                PWMs.append(int(3.7 * percent + 1160))
             elif percent == 100:
-                pwm = 1500
+                PWMs.append(1500)
 
-            PWMs.append(pwm)
+        self.pwm_2_thrust(PWMs)
 
     def pwm_2_thrust(self, PWMs):
+        self.THRUSTs = []
         for pwm in PWMs:
             if 1100 <= pwm and pwm < 1180:
                 thrust = 0.0258 * pwm - 37.5007
@@ -48,12 +65,20 @@ class Percentage2Thrust:
 
             else:
                 thrust = 0
-
             self.THRUSTs.append(thrust)
+        self.thruster_thrust_to_axe_thrust(self.THRUSTs)
 
+    def thruster_thrust_to_axe_thrust(self, thrust):
+        thrust_thruster = np.array(thrust)
+        thrust_axe = np.dot(thrust_thruster, self.thruster)
+        self.publish_data(thrust_axe, thrust)
 
+    def publish_data(self, thrust_axe, thrust_thruster):
+        msg = ThrustLogging()
+        msg.thrust_axe = list(thrust_axe)
+        msg.thrust_thruster = list(thrust_thruster)
 
+        self.thrust_publisher.publish(msg)
 
-
-
-
+if __name__ == '__main__':
+    thrust = Percentage2Thrust()
